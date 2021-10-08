64fe90ee1b4ce65e29ff919b37d6f217fd9399f8
Carl C <69673640+awsomesawce@users.noreply.github.com>
2021-10-07 21:18:57 -0400

    Update nodeHelpers.psm1

--- nodeHelpers.psm1
+++ nodeHelpers.psm1
@@ -5,3 +5,27 @@
 Helpers for NodeJS
 #>
 
+
+<#
+.Description
+Converts package.json file in current directory as a hashtable
+#>
+param([string]$File, [switch]$AsHashtable, [int]$Depth = 2)
+if ($File) {
+    if ($AsHashtable) {
+        $pkgjson = Get-Content $File | ConvertFrom-Json -AsHashtable -Depth $Depth
+        return $pkgjson
+    }
+    else {
+        $pkgjson = Get-Content $File | ConvertFrom-Json -Depth $Depth
+        return $pkgjson
+    }
+}
+else {
+    if (Test-Path ./package.json) {
+        $pkgjson = ConvertFrom-Json -InputObject "$(Get-Content .\package.json)" -AsHashtable -Depth $Depth
+        return $pkgjson
+    }
+    else { Write-Error "No ./package.json found in current dir" }
+}
+
